f068e31debd674449205347046647428f81dae31
jscpy <chuy.sc@hotmail.com>
2015-06-08 21:39:51 -0500

    mensaje

--- app/views/layouts/welcome.blade.php
+++ app/views/layouts/welcome.blade.php
@@ -4,33 +4,19 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" href="">    
-
-<title>Ciencias Básicas</title>      
-
-      <!-- CSS de Bootstrap -->
-      <link href="/css/bootstrap.min.css" rel="stylesheet" media="screen">
-      <link href="/css/bootstrap.css" rel="stylesheet" media="screen">
-      <link href="/css/bootstrap-theme.css" rel="stylesheet">
-      <link href="/css/jumbotron.css" rel="stylesheet">
-      <link href="/css/sticky-footer.css" rel="stylesheet">
-      <style type="text/css">
+    <link rel="shortcut icon" href=""> 
+    <style type="text/css">
         h2{text-align: center; color: white}
         body {background: url("/imagenes/colorful.jpg");}
         b{color: red;}
-      </style>
-
-      <!-- JS de Bootstrap -->
-      <script src="/js/jquery-1.11.0.js"></script>
-      <script src="/js/bootstrap.min.js"></script>
-      <script src="/js/deshabilitar.js"></script>
-      
+    </style>   
+<title>Ciencias Básicas</title>       
 </head>
 <body role="document">
 <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
       <div class="container">
        <div class="navbar-header">
-          <a class="navbar-brand" href="/">Ciencias Báscias 2014</a>
+          <a class="navbar-brand" href="/">Ciencias Básicas 2015</a>
         </div>
       </div>
 </div>
@@ -38,14 +24,16 @@
 <div class="container" >
 	@yield('content')
 </div>
-
-<!--   Pie de Pagina   
-
-<div id="footer">
-      <div class="container">
-        <p class="text-muted">Proyecto de Ciencias Básicas 2014.</p>
-        </div>
-    </div>
--->
 </body>
+      <!-- CSS de Bootstrap -->
+      <link href="/css/bootstrap.min.css" rel="stylesheet" media="screen">
+      <link href="/css/bootstrap.css" rel="stylesheet" media="screen">
+      <link href="/css/bootstrap-theme.css" rel="stylesheet">
+      <link href="/css/jumbotron.css" rel="stylesheet">
+      <link href="/css/sticky-footer.css" rel="stylesheet">
+
+      <!-- JS de Bootstrap -->
+      <script async src="/js/jquery-1.11.0.js"></script>
+      <script async src="/js/bootstrap.min.js"></script>
+      <script async src="/js/deshabilitar.js"></script>
 </html>
